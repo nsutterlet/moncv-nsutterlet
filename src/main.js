5894f0ca2adfa5a557da63962fe620d9423dcdfe
nsutterlet <n.sutterlet@outlook.com>
2017-06-29 12:02:10 +0200

    Remplacement des progress bar en Chart

--- src/main.js
+++ src/main.js
@@ -2,6 +2,47 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootswatch/superhero/bootstrap.min.css';
 import './main.css';
+import Chart from 'chart.js';
+
 $(document).ready(function () {
-    console.log('it works!');
+    transformPage();
+    console.log('it works!!');
 });
+
+/* eslint-disable no-new */
+/* Récupération des progress-bar */
+var listProgress = $('.progress');
+
+/* Extraction des données d'une progress-bar */
+function extractDataFromProgress (progress) {
+    const bar = progress.children[0];
+    const val = $(bar).attr('aria-valuenow');
+    return val;
+}
+
+/* Fonction de création du chart */
+function makeChart (c, val) {
+    let option = {
+        type: 'doughnut',
+        data: {
+
+            datasets: [
+                {
+                    data: [100 - val, val]
+                }
+            ]
+        },
+        width: 50
+    };
+    new Chart(c, option);
+}
+
+/* Tranformation de la page */
+function transformPage () {
+    for (let progress of listProgress) {
+        const val = extractDataFromProgress(progress);
+        const c = $('<canvas></canvas>');
+        $(progress).replaceWith(c);
+        makeChart(c, val);
+    }
+}
